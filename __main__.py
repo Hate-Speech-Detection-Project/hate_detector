b1597102dd485311c45d657893184bfc01c75304
Carl Ambroselli <git@carl-ambroselli.de>
2017-08-08 17:59:37 +0200

    Add word2vec features

--- __main__.py
+++ __main__.py
@@ -1,5 +1,18 @@
 import pandas as pd
 from predictor import Predictor
+import nltk
+import os
+
+def init_nltk():
+    if not os.path.exists('nltk'):
+        os.makedirs('nltk')
+    nltk.data.path.append(os.getcwd() + '/nltk')
+    dependencies = ['corpora/stopwords']
+    for package in dependencies:
+        try:
+            nltk.data.find(package)
+        except LookupError:
+            nltk.download(package, os.getcwd() + '/nltk')
 
 def load_data(dataset='tiny'):
     train_df = pd.read_csv('data/' + dataset + '/train.csv', sep=',')
@@ -7,6 +20,7 @@ def load_data(dataset='tiny'):
     return (train_df, test_df)
 
 def main():
+    init_nltk()
     print("Load Data...")
     train_df, test_df = load_data()
     predictor = Predictor()
---
+++ feature/word2vec.py
@@ -0,0 +1,59 @@
+from gensim.models import Word2Vec as gensim_word2vec
+from nltk.stem.snowball import SnowballStemmer
+from nltk.corpus import stopwords
+import numpy as np
+import re
+
+class Word2Vec:
+    def __init__(self):
+        # load model
+        self.w2v_model = gensim_word2vec.load('model/word2vec/all_lowercased_stemmed')
+        # initialize stemmer
+        self.stemmer = SnowballStemmer('german')
+        # grab stopword list
+        self.stop = stopwords.words('german')
+
+    def extractFeatures(self, df):
+        data = self.remove_stop_and_stem(df['comment'])
+        vectors = np.asarray(list(map(self.comment_to_vectors, data)))
+        return vectors
+
+
+    def text_to_wordlist(self, comment):
+        try:
+            comment_text = re.sub(r'https?:\/\/.*[\r\n]*', '', comment, flags=re.MULTILINE)
+            comment_text = re.sub(r'<\/?em>', '', comment_text, flags=re.MULTILINE)
+            comment_text = re.sub("[^a-zA-ZöÖüÜäÄß]"," ", comment_text)
+            comment_text = re.sub("\s\s+"," ", comment_text)
+            comment_text = comment_text.lower() + '. '
+        except:
+            comment_text = ''
+        return comment_text
+
+    def to_wordlist(self, data):
+        return data.apply(self.text_to_wordlist)
+
+    def remove_stopwords(self, data):
+        return data.apply(lambda x: [item for item in str(x).split(' ') if item not in self.stop])
+
+    def stem(self, data):
+        return data.apply(lambda x: " ".join([self.stemmer.stem(y) for y in x]))
+
+    def word_to_position(self, word):
+        try:
+            return self.w2v_model.wv[word]
+        except:
+            return -1
+
+    def comment_to_vectors(self, comment):
+        words = comment.split(' ')
+        result = list(map(self.word_to_position, words))
+        result = sum(result) / len(words)
+        return result
+
+    def remove_stop_and_stem(self, data):
+        data = self.to_wordlist(data)
+        data = self.remove_stopwords(data)
+        data = self.stem(data)
+        return data
+
