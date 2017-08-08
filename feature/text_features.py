77f2ee1ab030655855cea11baa28ca1accf2fd19
Carl Ambroselli <git@carl-ambroselli.de>
2017-08-08 16:23:22 +0200

    Initial boilerplate

---
+++ feature/text_features.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+class TextFeatures:
+
+  def extractFeatures(self, df):
+    total_length = df['comment'].apply(lambda x: len(x))
+    num_of_words = df['comment'].apply(lambda x: len(x.split()))
+    avg_length = df['comment'].apply(lambda x: np.average([len(a) for a in x.split()]))
+    num_questions = df['comment'].apply(lambda x: x.count('?'))
+    num_quote = df['comment'].apply(lambda x: x.count('"'))
+    num_dot = df['comment'].apply(lambda x: x.count('.'))
+    num_repeated_dot = df['comment'].apply(lambda x: x.count('..'))
+    num_exclamation = df['comment'].apply(lambda x: x.count('!'))
+    num_http = df['comment'].apply(lambda x: x.count('http'))
+    ratio_capitalized = df['comment'].apply(lambda x: sum(1 for c in x if c.isupper()) / len(x))
+
+    features = np.vstack((
+      total_length,
+      num_of_words,
+      avg_length,
+      num_questions,
+      num_quote,
+      num_dot,
+      num_repeated_dot,
+      num_exclamation,
+      num_http,
+      ratio_capitalize
+    )).T
+    return features
---
+++ predictor.py
@@ -0,0 +1,10 @@
+
+# Get all features from feature combiner and train the classifier(s)
+def fit(df):
+  pass
+
+# Predict if comments in dataframe are hate
+# returns dataframe containing one column with the result of each classifier and a column of the combined result
+def predict(df):
+  pass
+
