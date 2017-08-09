efa27f56d030bc43a204f3d6ad23892e40414d24
Carl Ambroselli <git@carl-ambroselli.de>
2017-08-09 16:37:35 +0200

    Add naive bayes and normalization

--- classifier/naive_bayes.py
+++ classifier/naive_bayes.py
@@ -1,14 +1,16 @@
-from sklearn.svm import SVR as sklearn_svr
+from sklearn.naive_bayes import MultinomialNB
 import numpy as np
 import pandas as pd
 
-class SVR:
+class NaiveBayes:
     def __init__(self):
-        self.svr = sklearn_svr(kernel='rbf')
+        self.nb = MultinomialNB()
         self.model = None
 
     def fit(self, features, ground_truth):
-        self.model = self.svr.fit(features, ground_truth)
+        print("Features")
+        print(np.min(features))
+        self.model = self.nb.fit(features, ground_truth)
 
     def predict(self, features):
         assert self.model is not None, 'Executed predict() without calling fit()'
