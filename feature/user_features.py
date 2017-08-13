931ba9adf01d822396e13bb20a91e5d0e1ea76ee
jan.ladleif <jan.ladleif@student.hpi.de>
2017-08-13 22:10:47 +0200

    add recent user features

--- feature/user_features.py
+++ feature/user_features.py
@@ -5,13 +5,16 @@ class UserFeatures:
   def extractFeatures(self, df):
     features = np.vstack((
       df['time_since_last_comment'],
+      df['time_since_last_hate_comment'],
       df['time_since_last_comment_same_user'],
       df['time_since_last_hate_comment_same_user'],
       df['time_since_last_comment_same_user_any_article'],
       df['time_since_last_hate_comment_same_user_any_article'],
+      df['time_since_article'],
       df['number_of_comments_by_user'],
+      df['number_of_comments_by_user_in_ressort'],
       df['number_of_hate_comments_by_user'],
-      df['share_of_hate_comments_by_user']
+      df['number_of_hate_comments_by_user_in_ressort']
     )).T
 
     return features
