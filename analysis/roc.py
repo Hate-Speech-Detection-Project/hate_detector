fe92c606436f5b14a15146a2e1deeaf9d433af68
Isfet <Isfet>
2017-08-23 16:48:49 +0200

    Update ROC curve plot

--- analysis/roc.py
+++ analysis/roc.py
@@ -30,7 +30,7 @@ class ROC:
                  lw=lw, label='ROC {} curve (area = %0.2f)'.format(label, roc_auc))
         plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
         plt.xlim([0.0, 1.0])
-        plt.ylim([0.0, 1.05])
+        plt.ylim([0.0, 1.0])
         plt.xlabel('False Positive Rate')
         plt.ylabel('True Positive Rate')
         plt.title('Receiver operating characteristic')
