9c765fa09a537c246c2ef9267390cdb262eff50c
Isfet <Isfet>
2017-09-04 14:14:38 +0200

    Add class_weight parameter to logistic regression

--- classifier/logistic_regression.py
+++ classifier/logistic_regression.py
@@ -4,5 +4,5 @@ from classifier.classifier import Classifier
 
 class LogisticRegression(Classifier):
     def __init__(self):
-        super().__init__(LogisticRegressionClassifier(random_state=0), useWeights=True, useCalibration=True)
+        super().__init__(LogisticRegressionClassifier(random_state=0, class_weight='balanced'), useWeights=True, useCalibration=True)
         self.name = "logistic regression"
